3a1555d2b5a2db75d34bc309e40ee84190dcecc4
Elijah <eramos@class-ic.com>
2026-03-04 15:29:07 -0800

    feat: main.cpp smoke test; initialize file

---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int, char**){
+    std::cout << "sat_sim build ok\n";
+    return 0;
+}
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+// #include "Orbit.hpp"     // you'll create this later
+
+int main() {
+    std::cout << "Satellite orbit simulator\n";
+
+    // TODO: initialize Earth, satellite, time step, etc.
+    // Example placeholders:
+    //   Orbit earthOrbit(earthMass, satelliteMass, initialPosition, initialVelocity);
+    //   while(simulationRunning) { earthOrbit.step(dt); }
+
+    return 0;
+}
