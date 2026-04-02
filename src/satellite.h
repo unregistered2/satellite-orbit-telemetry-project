1f85fddbb6457f4dc469ce27f9ce69c7bb7be661
Elijah <eramos@class-ic.com>
2026-04-02 11:32:52 -0700

    feat: define Satellite class with orbital state and methods

--- src/satellite.h
+++ src/satellite.h
@@ -1,7 +1,27 @@
 #pragma once
+#include "vec3.h"
 
 class Satellite
 {
-public:
-    Satellite();
+    public:
+        Satellite();
+        void update(double dt);
+
+        Vec3 position() const;
+        Vec3 velocity() const;
+
+        double altitude() const;
+        double speed() const;
+
+    private:
+        double earthRadius_;
+        double altitude_;
+        double mu_; // standard gravitational parameter
+
+        double radius_;
+        double speed_;
+        double omega_; // angular velocity
+
+        double theta_; // current angle in radians
+
 };
